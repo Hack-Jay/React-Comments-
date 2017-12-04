1dd21a03faeb70c274fb76580a1e047696d8b379
hack_jay <735059024@qq.com>
2017-12-04 13:48:23 +0800

    second project

---
+++ src/Comment.js
@@ -0,0 +1,62 @@
+import React,{Component} from "react"
+import PropTypes from 'prop-types';
+
+class Comment extends Component {
+  static propTypes={
+    comment:PropTypes.object.isRequired,
+    onDeleteComment:PropTypes.func,
+    index:PropTypes.number
+  }
+  constructor(){
+    super()
+    this.state={
+      timestring:''
+    }
+  }
+  componentWillMount () {
+    this._updateTimeString()
+    this._timer = setInterval(
+      this._updateTimeString.bind(this),
+      5000
+    )
+  }
+  componentWillUnmount () {
+    clearInterval(this._timer)
+  }
+  _updateTimeString () {
+    const comment = this.props.comment
+    const duration = (+Date.now() - comment.createdTime) / 1000
+    this.setState({
+      timeString: duration > 60
+        ? `${Math.round(duration / 60)} 分钟前`
+        : `${Math.round(Math.max(duration, 1))} 秒前`
+    })
+  }
+  handleDeleteComment () {
+    if (this.props.onDeleteComment) {
+      this.props.onDeleteComment(this.props.index)
+    }
+  }
+
+  render () {
+    const {comment} =this.props
+    return (
+      <div className='comment'>
+        <div className='comment-user'>
+          <span>{this.props.comment.username} </span>：
+        </div>
+        <p>{this.props.comment.content}</p>
+        <span className='comment-createdtime'>
+          {this.state.timeString}
+        </span>
+        <span
+        onClick={this.handleDeleteComment.bind(this)}
+         className='comment-delete'>
+          删除
+        </span>
+      </div>
+    )
+  }
+}
+
+export default Comment
